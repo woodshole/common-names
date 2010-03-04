64aa4bfb22718612152631485907b5ffefd6b08a
Anthony Goddard <agoddard@mbl.edu>
2010-03-04 16:23:27 -0500

    added pluralization of taxon

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,5 @@
 # Be sure to restart your server when you modify this file
+require 'active_support/inflector'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
@@ -66,4 +67,10 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+
+  #we gotta tell rails that taxa is the plural of taxon
+  ActiveSupport::Inflector.inflections do |inflect|
+    inflect.irregular 'taxon', 'taxa'
+  end
+
 end
