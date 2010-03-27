727141460b28ad5fefebc768f286f0f4498cd9f9
John Marino <writejm@gmail.com>
2010-03-26 22:35:39 -0400

    js methods can create their new names

--- app/controllers/common_names_controller.rb
+++ app/controllers/common_names_controller.rb
@@ -1,9 +1,20 @@
 class CommonNamesController < ApplicationController
   def create
+    
+    if params[:language]
+      if language = Language.find_by_iso_code(params[:language])
+        # found the language
+      else
+        render :text => "Failure: Language doesn't exist"
+      end
+    else
+      language = current_user.language
+    end
+    
     if @taxon = Taxon.find_by_name(params[:taxon_name])
-      common_name = CommonName.new(:name => params[:name], :taxon => @taxon, :language => current_user.language)
+      common_name = CommonName.new(:name => params[:name], :taxon => @taxon, :language => language)
       if common_name.save
-        @names = @taxon.common_names.all
+        @names = @taxon.language_common_names(language)
         render :partial => "taxa/list", :layout => false
       else
         render :text => "Failure: Name did not save."
