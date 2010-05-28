7981e60a06b9cc6cb721737a8bb62d7bdbafec5e
Ryan Schenk <rschenk@gmail.com>
2010-05-28 15:53:00 -0400

    Updated TaxaController#stats to use the new stats method on the Taxon model

--- app/controllers/taxa_controller.rb
+++ app/controllers/taxa_controller.rb
@@ -12,9 +12,8 @@ class TaxaController < ApplicationController
   
   def stats
     @taxon = Taxon.find(params[:taxon_id])
-    @languages = @taxon.get_languages_of_children
+    @language_coverage = @taxon.get_language_coverage
     @count = @taxon.get_number_of_children
-    # @percent_translated = @taxon.percent_of_children_with_common_name
-    render :partial => 'language/stats', :layout => false
+    render :partial => 'language/stats', :layout => false, :collection => @language_coverage
   end
 end
